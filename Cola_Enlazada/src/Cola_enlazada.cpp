9cda105242feae87d9a8944fc212a8bb36f2dece
Almudena Garcia <liberamenso10000@gmail.com>
2023-04-24 12:05:52 +0200

    cola_array: cambiada insercion para insertar al final

--- Cola_Enlazada/src/Cola_enlazada.cpp
+++ Cola_Enlazada/src/Cola_enlazada.cpp
@@ -6,18 +6,18 @@ Cola_enlazada::Cola_enlazada()
 }
 
 void Cola_enlazada::encolar(int dato){
-    this->cola.insertar_inicio(dato); //el inicio de la lista es la última posición de la cola
+    this->cola.insertar_final(dato); //el inicio de la lista es la última posición de la cola
     this->num_elementos = cola.longitud();
 }
 
 void Cola_enlazada::desencolar(){
-    this->cola.eliminar_final(); //el final de la lista es la primera posición de la cola
+    this->cola.eliminar_inicio(); //el final de la lista es la primera posición de la cola
     this->num_elementos = cola.longitud();
 }
 
 int Cola_enlazada::primero(){
     //El primer elemento de la lista será el que se encuentre en la última posición, dada por el número de elementos -1
-    return this->cola.obtener(this->num_elementos-1);
+    return this->cola.obtener(0);
 }
 
 bool Cola_enlazada::esvacia(){
